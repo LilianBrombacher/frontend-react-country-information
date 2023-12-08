c4cc0c5afdde8e38eeea58eeb802b412e5da0f15
LilianBrombacher <lbrombacher91@gmail.com>
2023-12-08 15:20:34 +0100

    button en eerste functie gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import './App.css';
+import axios from "axios";
+import world_map from './assets/world_map.png'
+import {useState} from "react";
 
 function App() {
+    const [countries, setCountries]= useState([]);
+
+    async function fetchCountries() {
+        try {
+            const response = await axios.get('https://restcountries.com/v3.1/all');
+            console.log(response.data);
+            setCountries(response.data);
+        } catch(e) {
+            console.error(e);
+            console.log('de statuscode van de fout is:', e.response.status);
+        }
+
+    }
 
     return (
         <>
-            Maak je applicatie hier!
+        <header>
+            <img className='header-image' src={world_map} alt="world_map"></img>
+        </header>
+        <main>
+            <button type='button' onClick={fetchCountries}>
+                Zoek alle landen
+            </button>
+            {countries.length > 0 && <ul>
+                {countries.map((country) => {
+                    return <li key={country}>{country}</li>
+                })}
+            </ul>}
+        </main>
         </>
     )
 }
